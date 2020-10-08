d1dfc05e8caf3e0e1cb4f7e478ad9c06296fad96
Robert Ahlberg <rob.ahlberg@gmail.com>
2020-10-08 18:57:40 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func validateEmail(email string) bool {
+	emailRegExp, _ := regexp.Compile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+	return emailRegExp.MatchString(email)
+}
+
+const fileName = "emails"
+
+func main() {
+	// Read emails
+	emails, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Could not read file", err.Error())
+		return
+	}
+	emailList := strings.Fields(string(emails))
+
+	// Add new email if arg
+	if len(os.Args) > 1 {
+		emailToAdd := os.Args[1]
+		if validateEmail(emailToAdd) {
+			emailList = append(emailList, emailToAdd)
+			// Write new email list
+			ioutil.WriteFile(fileName, []byte(strings.Join(emailList, "\n")), 0644)
+		} else {
+			fmt.Println("Email is not valid")
+			return
+		}
+	}
+
+	// Print the list
+	fmt.Printf("List: %v", emailList)
+}
